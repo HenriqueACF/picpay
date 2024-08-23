95c23be7883d7814962a044afe14ee73dc3c1c29
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-08-23 16:36:25 -0300

    feat: rota de criação e busca de usuario

--- src/main/java/com/picpay/domain/user/User.java
+++ src/main/java/com/picpay/domain/user/User.java
@@ -2,10 +2,7 @@ package com.picpay.domain.user;
 
 import com.picpay.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -15,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 

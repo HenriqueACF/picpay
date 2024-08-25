883f200788d1bb4c149a378199f31e32212f753f
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-08-25 18:55:46 -0300

    test: configurando banco de dados para testes

--- src/test/java/com/picpay/User/repository/UserRepositoryTest.java
+++ src/test/java/com/picpay/User/repository/UserRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.picpay.User.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Test

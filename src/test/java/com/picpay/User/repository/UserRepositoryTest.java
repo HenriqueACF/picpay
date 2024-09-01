d42fcbadc29ec33244f12517c7aea3b711d58d9a
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-09-01 12:37:20 -0300

    test: criando teste unitario no repository

--- src/test/java/com/picpay/User/repository/UserRepositoryTest.java
+++ src/test/java/com/picpay/User/repository/UserRepositoryTest.java
@@ -1,17 +1,74 @@
 package com.picpay.User.repository;
 
+import com.picpay.User.domain.User;
+import com.picpay.User.domain.UserType;
+import com.picpay.User.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.Optional;
 
 
 @DataJpaTest
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
     @Test
-    void findUserByDocument() {
+    @DisplayName("Deve retornar sucesso ao encontrar usuario")
+    void findUserByDocumentSuccess() {
+        String document = "12345678910";
+        UserDTO data = new UserDTO(
+                "Henrique",
+                "Assis",
+                document, new BigDecimal(10),
+                "henrique@test.com",
+                "Senha123#",
+                UserType.COMMON
+        );
+
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar erro quando nao encontrar usuario no banco de dados")
+    void findUserByDocumentError() {
+        String document = "12345678910";
+//        UserDTO data = new UserDTO(
+//                "Henrique",
+//                "Assis",
+//                document, new BigDecimal(10),
+//                "henrique@test.com",
+//                "Senha123#",
+//                UserType.COMMON
+//        );
+
+//        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    // cria novo usuario pra auxiliar o teste
+    private User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
     }
 }

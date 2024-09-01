b283428ad62607d5f7c98214305b2ebb8e73b662
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-09-01 11:49:31 -0300

    test: configurando banco de dados para teste

--- src/test/java/PicpayApplicationTests.java
+++ src/test/java/PicpayApplicationTests.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PicpaysimplificadoApplicationTests {
+class PicpayApplicationTests {
 
     @Test
     void contextLoads() {
